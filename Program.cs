14658226058579c93b85603d56115b592b1493af
sserditov <108936913+sserditov@users.noreply.github.com>
2022-08-09 15:43:58 +0500

    hometask6

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число a ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 1)
+{
+    Console.WriteLine("число нечетное");
+    else
+    {
+        Console.WriteLine("Число четное");
+    }
+}
